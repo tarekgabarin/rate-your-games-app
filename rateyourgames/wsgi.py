b652f490a008216c6c41a4f0a29944cf3aa2da1f
tarekgabarin <tarekgabarin1992@gmail.com>
2022-12-09 16:44:15 -0500

    beggining project

---
+++ rateyourgames/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for rateyourgames project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rateyourgames.settings')
+
+application = get_wsgi_application()
